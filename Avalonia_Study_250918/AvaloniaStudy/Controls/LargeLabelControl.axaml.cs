1fd717a44ecf08fc95e181772689a1decf2ab32a
Ma Merry <3267603516@qq.com>
2025-09-18 16:15:54 +0800

    迁移

---
+++ Avalonia_Study_250918/AvaloniaStudy/Controls/LargeLabelControl.axaml.cs
@@ -0,0 +1,29 @@
+﻿using Avalonia;
+using Avalonia.Controls.Primitives;
+
+namespace AvaloniaStudy.Controls;
+
+public class LargeLabelControl : TemplatedControl
+{
+    public static readonly StyledProperty<string> LargeTextProperty = AvaloniaProperty.Register<
+        LargeLabelControl,
+        string
+    >(nameof(LargeText), "Large Text Info");
+
+    public string LargeText
+    {
+        get => GetValue(LargeTextProperty);
+        set => SetValue(LargeTextProperty, value);
+    }
+
+    public static readonly StyledProperty<string> SmallTextProperty = AvaloniaProperty.Register<
+        LargeLabelControl,
+        string
+    >(nameof(SmallText), "Small Text Info");
+
+    public string SmallText
+    {
+        get => GetValue(SmallTextProperty);
+        set => SetValue(SmallTextProperty, value);
+    }
+}
